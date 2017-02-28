1d5871c76942ad72575a9ba29f330046894bedb7
Marvin Schön <m.schoen@lvm.de>
2017-02-28 14:07:06 +0100

    added supported app

--- samples/src/main/java/pferdi/PferdiSpeechletRequestStreamHandler.java
+++ samples/src/main/java/pferdi/PferdiSpeechletRequestStreamHandler.java
@@ -13,6 +13,7 @@ public class PferdiSpeechletRequestStreamHandler extends SpeechletRequestStreamH
 		 * Alexa Skill and put the relevant Application Ids in this Set.
 		 */
 		supportedApplicationIds.add("amzn1.ask.skill.b6eb7cbc-4878-4bad-be4d-2c82f0e6cae2");
+		supportedApplicationIds.add("amzn1.ask.skill.ac7a2dd6-b1dd-4858-8a71-4e62d0e2d399");
 	}
 
 	public PferdiSpeechletRequestStreamHandler() {
